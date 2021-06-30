000b48a0f2413bdc5f5d3e0421914bdf2ba5b620
Jithin M <Jithin.M@ltts.com>
2021-06-30 11:45:25 +0530

    Structure

---
+++ LMS_Cell_Dive_App/Constants/TestConstantscs.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LMS_Cell_Dive_App.Constants
+{
+    public static class TestConstantscs
+    {
+    }
+}
---
+++ LMS_Cell_Dive_App/Implementation/Test.cs
@@ -0,0 +1,12 @@
+﻿using LMS_Cell_Dive_App.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LMS_Cell_Dive_App.Implementation
+{
+    public class Test : ITest
+    {
+    }
+}
